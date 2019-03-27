6f659faa90a75b50d0501e0a2b96b3acbb7208ce
BuiUyen <UYEN@DESKTOP-0DMNRC3>
2019-03-27 20:53:52 +0700

    1

--- SapXepTen/Program.cs
+++ SapXepTen/Program.cs
@@ -24,7 +24,6 @@ namespace SapXepTen
             new HocSinh () { HoTen= "Pham Duc Huy",Tuoi = 5,GioiTinh= "nam"},
             new HocSinh () { HoTen= "Nguyen Thi Huyen",Tuoi = 8,GioiTinh= "nu"},
             new HocSinh () { HoTen= "Pham Thi Chinh",Tuoi = 22,GioiTinh= "nu"},
-
             };
             //Cat lay ten de sap xep theo ten
             foreach (HocSinh HS in mHocSinh)
